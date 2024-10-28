c6a8d053fd6e16bc5b0f482b07ddf20a463c0de9
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2024-10-27 22:43:50 -0700

    Update Records.tsx

--- client/src/components/Records.tsx
+++ client/src/components/Records.tsx
@@ -24,11 +24,14 @@ const Records: React.FC<RecordsProps> = ({ rec, refFunction }) => {
   useEffect(() => {
     if(refFunction) refFunction(ref.current)
   }, [ref.current])
+if(rec[0].level) {
+  rec.sort((a,b) => a.levelID.position - b.levelID.position)
+}
   return (
     <div className='overflow-x-hidden w-full' ref={ref}>
     <Table>
       <TableBody>
-        {rec.map((r, i) => (
+        {rec.filter(e => e.levelID.position < 101).map((r, i) => (
           <TableRow key={`record-${i}`} style={{backgroundColor: i % 2 ? "whitesmoke" : "lightgray"}} className={cn('text-lg', r.hertz === 60 && 'font-semibold')}>
             <TableCell>{r.level ? r.levelID.position : i+1}. {r.player ? <a href={`/#/leaderboard?player=${r.player}`}>{r.player}</a> : <a href={`/#/?level=${r.level}`}>{r.level}</a>}</TableCell>
             <TableCell style={{width: "20px"}}>{r.hertz}</TableCell>
